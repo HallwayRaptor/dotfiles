7cefeb12eb6dcf70089781e795277d706d90919e
Rob <hallwayraptor@gmail.com>
2022-07-05 09:54:57 -0300

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#update
+sudo pacman -Syu
+
+
+Install programs
+sudo pacman -S firefox steam npm thunderbird kitty git --needed base-devel flatpak discord --noconfirm
+
+# install paru 
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd
+
+# install from AUR
+paru -S visual-studio-code-bin   --noconfirm
+
+# download dotfiles
+git clone https://github.com/HallwayRaptor/dotfiles
+mkdir ~/.config/
+mv kitty ~/.config/
